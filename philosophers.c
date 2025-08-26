10d908900bdafcaa90dbaabde639d467ab79e058
Davit Grigoryan <dgrigor2@c1r3s5.42yerevan.am>
2025-08-26 18:14:09 +0400

    Kareni jany sax ylni

--- philosophers.c
+++ philosophers.c
@@ -6,12 +6,33 @@
 /*   By: dgrigor2 <dgrigor2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 16:36:17 by dgrigor2          #+#    #+#             */
-/*   Updated: 2025/08/23 18:20:17 by dgrigor2         ###   ########.fr       */
+/*   Updated: 2025/08/26 18:05:31 by dgrigor2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 #include <sys/wait.h>
+
+size_t	get_cur_time(void)
+{
+	struct timeval	currtime;
+
+	if (gettimeofday(&currtime, NULL) == -1)
+	{
+		printf("get time erreo\n");
+		return (0);
+	}
+	return (currtime.tv_sec * 1000 + currtime.tv_usec / 1000);
+}
+
+void	ft_usleep(size_t milisec)
+{
+	size_t	start;
+
+	start = get_cur_time();
+	while (milisec > get_cur_time() - start)
+		usleep(500);
+}
 
 void	*aristotle(void *plato_cpy)
 {
@@ -22,28 +43,41 @@ void	*aristotle(void *plato_cpy)
 	i = 0;
 	plato = (t_plato *)plato_cpy;
 	conds = plato->conds_cpy;
+	if (plato->num % 2 == 0)
+		ft_usleep((conds->eat / 2));
 	pthread_mutex_lock(conds->writing);
 	while (*plato->status)
 	{
 		pthread_mutex_unlock(conds->writing);
-		ft_print(*plato, 4);//think
 		pthread_mutex_lock(plato->first);
 		ft_print(*plato, 1);//fork
-		pthread_mutex_lock(plato->second);
-		ft_print(*plato, 1);//fork
-		ft_print(*plato, 2);//eating
-		pthread_mutex_lock(conds->writing);
-		gettimeofday(&(plato->last_eat), NULL);
-		pthread_mutex_unlock(conds->writing);
-		usleep(conds->eat * 1000);
-		ft_print(*plato, 3);//sleep
-		pthread_mutex_unlock(plato->second);
+		if (conds->n > 1)
+		{
+			pthread_mutex_lock(plato->second);
+			ft_print(*plato, 1);//fork
+			ft_print(*plato, 2);//eating
+			pthread_mutex_lock(conds->writing);
+			gettimeofday(&(plato->last_eat), NULL);
+			pthread_mutex_unlock(conds->writing);
+			ft_usleep(conds->eat);
+			ft_print(*plato, 3);//sleep
+			pthread_mutex_unlock(plato->second);
+		}
+		else
+			ft_usleep(conds->die + 1);
 		pthread_mutex_unlock(plato->first);
-		usleep(conds->sleep * 1000);
 		pthread_mutex_lock(conds->writing);
+		if (*plato->status)
+		{
+			pthread_mutex_unlock(conds->writing);
+			ft_usleep(conds->sleep);
+			ft_print(*plato, 4);//think
+			// if (conds->die - conds->eat)
+			// 	ft_usleep((conds->die - conds->eat) * 200);
+			pthread_mutex_lock(conds->writing);
+		}
 	}
 	pthread_mutex_unlock(conds->writing);
-	write(1, "return el em arel, inch es uzum?\n", 34);
 	return (NULL);
 }
 
@@ -56,8 +90,8 @@ void	check_death(t_conds conds, t_plato *plato)
 	pthread_mutex_lock(conds.writing);
 	while (*plato[0].status)
 	{
-		pthread_mutex_unlock(conds.writing);
 		i = 0;
+		pthread_mutex_unlock(conds.writing);
 		while (i < conds.n)
 		{
 			pthread_mutex_lock(conds.writing);
@@ -71,7 +105,7 @@ void	check_death(t_conds conds, t_plato *plato)
 				pthread_mutex_unlock(conds.writing);
 			i++;
 		}
-		usleep(500);
+		ft_usleep(10);
 		pthread_mutex_lock(conds.writing);
 	}
 	pthread_mutex_unlock(conds.writing);
@@ -85,21 +119,22 @@ int main(int argc, char **argv)
 	pthread_mutex_t *mutex;
 	int status;
 
-	parser(&conds, argc, argv);
+	if (!parser(&conds, argc, argv))
+		return (0);
 	status = 1;
 	mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * conds.n);
 	if (!mutex)
 		return (0);
-	i = 0;
-	while (i < conds.n)
-	{
+	conds.writing = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	if (!conds.writing)
+		return (free(mutex), 1);
+	i = -1;
+	while (++i < conds.n)
 		pthread_mutex_init(&(mutex[i]), NULL); // check for fail
-		i++;
-	}
 	pthread_mutex_init(conds.writing, NULL); //and possibly here
 	plato = (t_plato *)malloc(sizeof(t_plato) * conds.n);
 	if (!plato)
-		return (free(mutex), 0);
+		return (free(mutex), free(conds.writing), 0);
 	gettimeofday(&conds.starttime, NULL);
 	i = 0;
 	while (i < conds.n)
@@ -108,40 +143,31 @@ int main(int argc, char **argv)
 		plato[i].conds_cpy = &conds;
 		plato[i].status = &status;
 		plato[i].num = i;
-		if (i % 2 == 0)
+		if (1)
 		{
 			plato[i].f = i;
 			plato[i].s = i + 1;
 			if (i >= conds.n - 1)
 				plato[i].s = 0;
 		}
-		else
-		{
-			plato[i].f = i + 1;
-			plato[i].s = i;
-			if (i >= conds.n - 1)
-				plato[i].f = 0;
-		}
+		// else
+		// {
+		// 	plato[i].f = i + 1;
+		// 	plato[i].s = i;
+		// 	if (i >= conds.n - 1)
+		// 		plato[i].f = 0;
+		// }
 		plato[i].first = &(mutex[plato[i].f]);
 		plato[i].second = &(mutex[plato[i].s]);
-		// if (i % 2 == 1)
-		// 	usleep(100);
-		pthread_create(&plato[i].id, NULL, aristotle, &(plato[i]));
 		i++;
 	}
+	i = -1;
+	while (++i < conds.n)
+		pthread_create(&plato[i].id, NULL, aristotle, &(plato[i]));
 	check_death(conds, plato);
 	i = 0;
-	write(1, "ancanq\n", 7);
-	printf("%d\n", conds.n);
 	while (i < conds.n)
-	{
-		pthread_mutex_lock(conds.writing);
-		write(1, "mta, harcer?\n", 14);
-
-		pthread_mutex_unlock(conds.writing);
 		pthread_join(plato[i++].id, NULL);
-		write(1, "Som Ting Wong\n", 15);
-	}
 	free(mutex);
 	free(plato);
 	return (0);
